23739bcb0ba320cc26c27b74d4e6faa031a75505
rod-farva-sql <33871765+rod-farva-sql@users.noreply.github.com>
2023-10-26 09:34:06 -0500

    Update revoke_ovpn_cert.py

--- playbooks/openvpn/revoke_ovpn_cert.py
+++ playbooks/openvpn/revoke_ovpn_cert.py
@@ -61,7 +61,7 @@ def main():
     parser.add_argument('--slack_token', type=str, help='Provide the slack token', required=True)
 
     # Configure logging settings
-    log_filename = '/var/log/openvpn/revoke_cert.log'
+    log_filename = '/var/log/openvpn/revoke_ovpn_cert.log'
     logging.basicConfig(filename=log_filename,
                     level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
